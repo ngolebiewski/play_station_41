8f2667491e4d4fb0254feaf7145cdccf81e7accd
Nick Golebiewski <nick.golebiewski@gmail.com>
2026-03-12 16:58:50 -0400

    🖼️ Art start with Asesprite and Tiled - W.I.P. art files

---
+++ tiled_files/CLASSROOM.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="CLASSROOM" tilewidth="16" tileheight="16" tilecount="63" columns="9">
+ <image source="../aseprite_art/CLASSROOM.png" trans="ff00ff" width="144" height="112"/>
+</tileset>
